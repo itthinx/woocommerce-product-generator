c673d49bd7ed7667e6468e3cf5b9758c30c39352
Kathy Darling <helgatheviking@gmail.com>
2018-02-19 13:28:21 -0600

    move dummy content to text fields and read file contents

--- woocommerce-product-generator.php
+++ woocommerce-product-generator.php
@@ -45,508 +45,9 @@ class WooCommerce_Product_Generator {
 
 	const DEFAULT_LIMIT = 10000;
 
-	const DEFAULT_TITLES =
-'
-ACME
-Banana
-Apple
-Carrot
-Tomato
-Potato
-Soy
-Strawberry
-Pumpkin
-Juice
-iPhone
-Mac
-Galaxy
-Album
-Jazz
-Samsung
-Motorola
-Egg
-Shampoo
-Shower
-Gel
-Moustache
-Bikini
-Towel
-Cool
-CD
-DVD
-Movie
-Shoe
-Dress
-Trouser
-Knife
-Doll
-Download
-Video
-Book
-Sword
-Ring
-Card
-Princess
-Prince
-Car
-Toy
-Fire
-HDMI
-TV
-Player
-Film
-Gift
-Wet
-Gun
-Water
-Beverage
-Cola
-Smoothie
-Washer
-Oven
-Refrigerator
-Internet
-Heater
-Cooler
-Cleaner
-Fish
-Meat
-Hair
-Lotion
-Mixer
-Blender
-Diamond
-Perl
-Thermal
-Rooster
-Chicken
-Lamb
-Hot
-Cold
-Microwave
-Natural
-Wood
-Plastic
-Chair
-Table
-Cover
-Sheet
-Bed
-Cushion
-Seat
-Bike
-Kit
-Gold
-Silver
-Brass
-Red
-Green
-Blue
-Yellow
-Black
-White
-Pink
-Violet
-Purple
-Lime
-Lavender
-Brown
-Grey
-Small
-Medium
-Large
-';
-
-	const DEFAULT_CONTENTS = '
-Lorem Ipsum Dolor Sit Amet
-Sed ut perspiciatis unde omnis iste natus error sit voluptatem.
-Do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque.
-Nisi ut aliquid ex ea commodi consequatur?
-Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.
-Do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-Nisi ut aliquid ex ea commodi consequatur?
-Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam.
-Corrupti quos dolores et quas molestias excepturi sint occaecati.
-Itaque earum rerum hic tenetur a sapiente delectus.
-Non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
-
-You mean it controls your actions?
-Partially, but it also obeys your commands. What?!
-I suggest you try it again, Luke.
-This time, let go your conscious self and act on instinct.
-Escape is not his plan.
-I must face him, alone.
-I\'m surprised you had the courage to take the responsibility yourself.
-Don\'t act so surprised, Your Highness.
-You weren\'t on any mercy mission this time.
-Several transmissions were beamed to this ship by Rebel spies.
-I want to know what happened to the plans they sent you.
-But with the blast shield down, I can\'t even see!
-How am I supposed to fight?
-I want to come with you to Alderaan.
-There\'s nothing for me here now.
-I want to learn the ways of the Force and be a Jedi, like my father before me.
-Obi-Wan is here.
-The Force is with him.
-I need your help, Luke.
-She needs your help.
-I\'m getting too old for this sort of thing.
-I\'m surprised you had the courage to take the responsibility yourself.
-Don\'t act so surprised, Your Highness.
-You weren\'t on any mercy mission this time.
-Several transmissions were beamed to this ship by Rebel spies.
-I want to know what happened to the plans they sent you.
-Hokey religions and ancient weapons are no match for a good blaster at your side, kid.
-You\'re all clear, kid.
-Let\'s blow this thing and go home!
-As you wish.
-I want to come with you to Alderaan.
-There\'s nothing for me here now.
-I want to learn the ways of the Force and be a Jedi, like my father before me.
-What?!
-What good is a reward if you ain\'t around to use it?
-Besides, attacking that battle station ain\'t my idea of courage.
-It\'s more like ... suicide.
-Ye-ha!
-
-The swallow may fly south with the sun, and the house martin or the plover may seek warmer climes in winter, yet these are not strangers to our land.
-On second thoughts, let\'s not go there.
-It is a silly place.
-Why do you think that she is a witch?
-The Knights Who Say Ni demand a sacrifice!
-Strange women lying in ponds distributing swords is no basis for a system of government.
-Supreme executive power derives from a mandate from the masses, not from some farcical aquatic ceremony.
-It\'s only a model.
-What do you mean?
-The nose?
-
-All I want is to be a monkey of moderate intelligence who wears a suit ... that\'s why I\'m transferring to business school!
-Shut up and get to the point!
-No!
-Don\'t jump!
-The kind with looting and maybe starting a few fires!
-In your time, yes, but nowadays shut up!
-Besides, these are adult stemcells, harvested from perfectly healthy adults whom I killed for their stemcells.
-It\'s just like the story of the grasshopper and the octopus.
-All year long, the grasshopper kept burying acorns for winter, while the octopus mooched off his girlfriend and watched TV.
-
-While their products leave much to be desired, Acme delivery service is second to none;
-Wile E. can merely drop an order into a mailbox (or enter an order on a website, as in the Looney Tunes: Back in Action movie), and have the product in his hands within seconds.
-The name Acme became popular for businesses by the 1920s, when alphabetized business telephone directories such as the Yellow Pages began to be widespread.
-
-A battery, providing the power source for the phone functions.
-An input mechanism to allow the user to interact with the phone.
-The most common input mechanism is a keypad, but touch screens are also found in some high-end smartphones.
-A screen which echoes the user\'s typing, displays text messages, contacts and more.
-Basic mobile phone services to allow users to make calls and send text messages.
-All GSM phones use a SIM card to allow an account to be swapped among devices.
-Some CDMA devices also have a similar card called a R-UIM.
-Individual GSM, WCDMA, iDEN and some satellite phone devices are uniquely identified by an International Mobile Equipment Identity (IMEI) number.
-FooPhone is an attempt to develop a mobile phone which contains only Foo.
-
-Caution! May contain Peanuts!
-
-In common language usage, "fruit" normally means the fleshy seed-associated structures of a plant that are sweet or sour and edible in the raw state, such as apples, oranges, grapes, strawberries, bananas, and lemons.
-On the other hand, the botanical sense of "fruit" includes many structures that are not commonly called "fruits", such as bean pods, corn kernels, wheat grains, and tomatoes.
-
-Some vegetables also contain fiber, important for gastrointestinal function.
-Vegetables contain important nutrients necessary for healthy hair and skin as well.
-A person who refrains from dairy and meat products, and eats only plants (including vegetables) is known as a vegan.
-Some vegetables can be consumed raw, while some, such as cassava, must be cooked to destroy certain natural toxins or microbes in order to be edible.
-A number of processed food items available on the market contain vegetable ingredients and can be referred to as "vegetable derived" products.
-These products may or may not maintain the nutritional integrity of the vegetable used to produce them.
-Of all the world\'s nations, China is the leading cultivator of vegetables, with top productions in potato, onions, cabbage, lettuce, tomatoes and broccoli.
-The tomato is consumed in diverse ways, including raw, as an ingredient in many dishes, sauces, salads, and drinks.
-While it is botanically a fruit, it is considered a vegetable for culinary purposes (as well as under U.S. customs regulations, see Nix v. Hedden), which has caused some confusion.
-The fruit is rich in lycopene, which may have beneficial health effects.
-
-iPhone is a line of smartphones designed and marketed by Apple Inc.
-It runs Apple\'s iOS mobile operating system.
-The first generation iPhone was released on June 29, 2007; the most recent iPhones, the seventh-generation iPhone 5C and iPhone 5S, were introduced on September 10, 2013.
-The MacBook Pro is a line of Macintosh portable computers introduced in January 2006 by Apple Inc., and now in its third generation.
-Replacing the PowerBook G4, the MacBook Pro was the second model, after the iMac, to be announced in the Apple-Intel transition.
-It is also the high-end model of the MacBook family and is currently produced with 13- and 15-inch screens, although a 17-inch version has been offered previously.
-
-';
-	
-	const DEFAULT_CATEGORIES = 'Action Figures
-All Action Figures
-Accessories
-Animals
-Collectibles
-Military Figures
-Playsets
-Police, Fire & Rescue Figures
-Political Figures
-Robots
-Science Fiction & Fantasy Figures
-Sports Figures
-Statues, Maquettes & Busts
-Activities & Amusements
-All Activities & Amusements
-Bath Toys
-Bubbles
-Drawing Tablet Toys
-Finger Puppets
-Finger Toys
-Flying Toys
-Fortunetelling Toys
-Gags & Practical Jokes
-Glow in the Dark Toys
-Inflatable Toys
-Juggling Sets
-Light-Up Toys
-Magnets
-Miniatures & Keychains
-Nesting Dolls
-Noisemakers
-Novelty Games
-Popping & Jumping Toys
-Prisms & Kaleidoscopes
-Slime & Putty Toys
-Spinning Tops
-Spring & Wind-up Toys
-Temporary Tattoos
-Toy Balls
-Viewfinders
-Water Toys
-Yo-yos
-Arts & Crafts
-All Arts & Crafts
-Aprons & Smocks
-Art Supply Sets & Kits
-Beads & Jewelry
-Chalk & Chalkboards
-Clay, Dough & Pottery
-Craft Kits
-Crayons
-Drawing & Sketch Pads
-Drawing & Sketching Tablets
-Dry Erase Markers & Boards
-Easels
-Glue, Paste & Tape
-Paintbrushes
-Paints
-Paper
-Pencils & Erasers
-Pens & Markers
-Scissors
-Sharpeners
-Stamps & Stickers
-Bikes, Skates & Ride-Ons
-All Bikes, Skates & Ride-Ons
-Bikes
-Skates
-Skateboards
-Scooters
-Ride-On Toys
-Safety Equipment
-Construction, Blocks & Models
-All Construction, Blocks & Models
-Blocks
-Building Sets
-Construction & Models
-Dolls
-All Dolls
-Accessories
-Baby Dolls
-Dollhouses
-Ethnic Dolls
-Fashion Dolls
-Interactive Dolls
-Playsets
-Porcelain Dolls
-Rag Dolls
-Toddler Dolls
-Electronics for Kids
-All Electronics for Kids
-Alarm Clocks
-Arcade Games
-Audio & Video Players
-Cameras
-Electronic Pets
-Handheld Games
-Learning & Education
-Lighting & Light Makers
-Organizers & PDA\'s
-Personal Video Players & Accessories
-Play Laptops & Notebooks
-Plug & Play Video Games
-RC Figures & Robots
-Safes & Security Devices
-Spy Gadgets
-TVs
-Telephones
-Virtual Reality
-Walkie Talkies
-Watches
-Games
-All Games
-Accessories
-Action & Reflex Games
-Backyard Games
-Battling Tops
-Board Games
-Card Games
-Casino Games
-Checkers, Chess & Backgammon
-Classic Games
-DVD Games
-Dice & Marble Games
-Dominoes & Tile Games
-Educational Games
-Electronic
-Floor Games
-Foreign Language Games
-Game Room Games
-Hasbro Games
-Mystery Games
-Religious Games
-Sports Games
-Stacking Games
-Travel Games
-TV Games
-Variety Game Sets
-Hobbies
-All Hobbies
-Coin Collecting
-Die-Cast
-Hobby Tools
-Models
-Radio Control
-Rockets
-Science
-Sports Trading Cards
-Trains
-Kids\' Furniture & Room Décor
-All Kids\' Furniture & Room Décor
-Activity & Sensory Tables
-Bedroom Furniture
-Chairs & Sofas
-Desks
-Outdoor Furniture
-Room Décor
-Step Stools
-Tables
-Tents & Tunnels
-Toy Chests & Storage
-Learning & Exploration
-All Learning & Exploration
-Early Development Toys
-Electronic Learning
-Foreign Languages
-Geography & Globes
-History
-Math & Counting
-Reading & Writing
-Science
-Music
-All Music
-Dance Mats
-Karaoke
-Musical Instruments
-Radio & CD Players
-Party Supplies
-All Party Supplies
-Cake Supplies
-Decorations
-Favors
-Hats
-Invitations & Cards
-Party Games & Crafts
-Party Packs
-Piñatas
-Tableware
-Play Vehicles
-All Play Vehicles
-Boats
-Buses
-Cars & Playsets
-Construction & Farm Vehicles
-Emergency Vehicles
-Military Vehicles
-Motorcycles
-Planes & Helicopters
-Radio & Remote Control
-Spacecraft
-Trains & Railway Sets
-Trucks & SUV\'s
-Vehicle Playsets
-Wood Vehicles
-Preschool
-All Preschool
-Baby Toys
-Toddler Toys
-Pre-Kindergarten Toys
-Pretend Play & Dress-up
-All Pretend Play & Dress-up
-Beauty & Fashion
-Costumes
-Pretend Electronics
-Puppets & Puppet Theaters
-Sets
-Spy Gear
-Puzzles
-All Puzzles
-Brain Teasers
-Floor Puzzles
-Foam Puzzles
-Jigsaw Puzzles
-Storage & Accessories
-Travel Puzzles
-Wood Puzzles
-Sports & Outdoor Play
-All Sports & Outdoor Play
-Ball Pits & Accessories
-Baseball & Softball
-Basketball
-Boxing
-Fishing Combo\'s & Accessories
-Fitness Equipment
-Football
-Golf
-Gymnastics
-Hockey
-Kites & Wind Spinners
-Lawn Games
-Outdoor Furniture
-Play Tents & Tunnels
-Playhouses
-Pogo Sticks & Hoppers
-Pools & Water Fun
-Sandbox & Beach
-Skateboarding
-Slumber Bags
-Snow Sports
-Soccer
-Swings, Gym Sets & Slides
-Tennis
-Toy Sports
-Trampolines & Inflatable Bouncers
-Volleyball
-Stuffed Animals & Toys
-All Stuffed Animals & Toys
-Animals
-Backpacks & Accessories
-Interactive
-More Stuffed Toys
-Movie & TV
-Plush Puppets
-Teddy Bears
-Toy Figures & Playsets
-All Toy Figures & Playsets
-Animals
-Bendable Figures
-Bobble Head Figures
-Construction
-Fantasy & Adventure
-Figure Accessories
-Military & Rescue
-Miniature Figures
-Movie & TV
-Playsets
-Vehicles';
+	private static $default_titles = '';
+	private static $default_contents = '';
+	private static $default_categories = '';
 
 	/**
 	 * Initialize hooks.
@@ -661,16 +162,16 @@ Vehicles';
 			add_option( 'woocommerce-product-generator-per-run', self::DEFAULT_PER_RUN, null, 'no' );
 
 			delete_option( 'woocommerce-product-generator-titles' );
-			add_option( 'woocommerce-product-generator-title', self::DEFAULT_TITLES, null, 'no' );
+			add_option( 'woocommerce-product-generator-title', self::get_default_titles(), null, 'no' );
 
 			delete_option( 'woocommerce-product-generator-contents' );
-			add_option( 'woocommerce-product-generator-contents', self::DEFAULT_CONTENTS, null, 'no' );
+			add_option( 'woocommerce-product-generator-contents', self::get_default_contents(), null, 'no' );
 		}
 
 		$limit    = get_option( 'woocommerce-product-generator-limit', self::DEFAULT_LIMIT );
 		$per_run  = get_option( 'woocommerce-product-generator-per-run', self::DEFAULT_PER_RUN );
-		$titles   = stripslashes( get_option( 'woocommerce-product-generator-titles', self::DEFAULT_TITLES ) );
-		$contents = stripslashes( get_option( 'woocommerce-product-generator-contents', self::DEFAULT_CONTENTS ) );
+		$titles   = stripslashes( get_option( 'woocommerce-product-generator-titles', self::get_default_titles() ) );
+		$contents = stripslashes( get_option( 'woocommerce-product-generator-contents', self::get_default_contents() ) );
 
 		$titles = explode( "\n", $titles );
 		sort( $titles );
@@ -910,7 +411,7 @@ Vehicles';
 
 			// add categories
 			$terms = array();
-			$cats = explode( "\n", self::DEFAULT_CATEGORIES );
+			$cats = explode( "\n", self::get_default_categories() );
 			$c_n = count( $cats );
 			$c_max = rand( 1, 3 );
 			for ( $i = 0; $i < $c_max ; $i++ ) {
@@ -1002,7 +503,7 @@ Vehicles';
 	 * @return string
 	 */
 	public static function get_title( $n_words = 3 ) {
-		$titles = trim( stripslashes( get_option( 'woocommerce-product-generator-titles', self::DEFAULT_TITLES ) ) );
+		$titles = trim( stripslashes( get_option( 'woocommerce-product-generator-titles', self::get_default_titles() ) ) );
 		$titles = explode( "\n", $titles );
 		$title = array();
 		$n = count( $titles );
@@ -1022,7 +523,7 @@ Vehicles';
 	 */
 	public static function get_excerpt( $n_lines = 3, $contents = null ) {
 		if ( $contents === null ) {
-			$contents = trim( stripslashes( get_option( 'woocommerce-product-generator-contents', self::DEFAULT_CONTENTS ) ) );
+			$contents = trim( stripslashes( get_option( 'woocommerce-product-generator-contents', self::get_default_contents() ) ) );
 		} else {
 			$contents = str_ireplace( '</p>', "\n", $contents );
 			$contents = str_ireplace( '<p>', '', $contents );
@@ -1048,7 +549,7 @@ Vehicles';
 	 * @return string
 	 */
 	public static function get_content( $n_lines = 10 ) {
-		$contents = trim( stripslashes( get_option( 'woocommerce-product-generator-contents', self::DEFAULT_CONTENTS ) ) );
+		$contents = trim( stripslashes( get_option( 'woocommerce-product-generator-contents', self::get_default_contents() ) ) );
 		$contents = explode( "\n", $contents );
 		$content = array();
 		$n = count( $contents );
@@ -1128,5 +629,48 @@ Vehicles';
 		}
 		return in_array( 'woocommerce/woocommerce.php', $active_plugins ); 
 	}
+
+	/**
+	 * Get default titles from file.
+	 * @return string
+	 */
+	private static function get_default_titles() {
+		if( ! self::$default_titles ) {
+			$content = file_get_contents( plugin_dir_path( __FILE__ ) . 'dummy-content/dummy-titles.txt' );
+			if( $content !== false ) {
+				self::$default_titles = $content;
+			}
+		}
+		return self::$default_titles;
+	}
+
+	/**
+	 * Get default content from file.
+	 * @return string
+	 */
+	private static function get_default_contents() {
+		if( ! self::$default_contents ) {
+			$content = file_get_contents( plugin_dir_path( __FILE__ ) . 'dummy-content/dummy-contents.txt' );
+			if( $content !== false ) {
+				self::$default_contents = $content;
+			}
+		}
+		return self::$default_contents;
+	}
+
+	/**
+	 * Get default categories from file.
+	 * @return string
+	 */
+	private static function get_default_categories() {
+		if( ! self::$default_categories ) {
+			$content = file_get_contents( plugin_dir_path( __FILE__ ) . 'dummy-content/dummy-categories.txt' );
+			if( $content !== false ) {
+				self::$default_categories = $content;
+			}
+		}
+		return self::$default_categories;
+	}
+
 }
 add_action( 'woocommerce_loaded', array( 'WooCommerce_Product_Generator', 'init' ) );
