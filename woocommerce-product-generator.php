b4b080354be70b6d05513c449d773f53c2ce9d67
itthinx <itthinx@itthinx.com>
2022-08-09 13:48:48 +0200

    update

--- woocommerce-product-generator.php
+++ woocommerce-product-generator.php
@@ -2,7 +2,7 @@
 /**
  * woocommerce-product-generator.php
  *
- * Copyright (c) 2014 "kento" Karim Rahimpur www.itthinx.com
+ * Copyright (c) 2014-2022 "kento" Karim Rahimpur www.itthinx.com
  *
  * This code is released under the GNU General Public License.
  * See COPYRIGHT.txt and LICENSE.txt.
